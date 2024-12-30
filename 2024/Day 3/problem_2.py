d0f97652bced096a9e33c60121a151e4aa2a984a
OMANOMNOM <jonathan.r1997@gmail.com>
2024-12-30 12:23:18 +0000

    Finished Day 3

--- 2024/Day 3/problem_2.py
+++ 2024/Day 3/problem_2.py
@@ -34,23 +34,27 @@ total_safe_reports = 0
 total_enabled = 0
 total_disabled = 0
 
-for line in f:
+line = ""
+for l in f:
+    line += l
+
+
     
-    do_cmd = get_search_indicies("do\(\)")
-    dont_cmd = get_search_indicies("don't\(\)")
+do_cmd = get_search_indicies("do\(\)")
+dont_cmd = get_search_indicies("don't\(\)")
 
 
-    for command in re.finditer("mul\(\d{1,3},\d{1,3}\)", line):
-        if get_highest_index_below_value(command.start(), do_cmd) > get_highest_index_below_value(command.start(), dont_cmd):
-            nums = re.findall("[0-9]+", line[command.start(): command.end()])
-            total_enabled += (int(nums[0]) * int(nums[1]))
-        else:
-            nums = re.findall("[0-9]+", line[command.start(): command.end()])
-            total_disabled += (int(nums[0]) * int(nums[1]))
-    print(f"Total enabled is :{total_enabled}")   
-    print(f"Total disabled is :{total_disabled}")  
-    print(f"Total added together is {total_enabled + total_disabled}")
-    print(f"We are off by {162813399 - (total_enabled + total_disabled)}") 
+for command in re.finditer("mul\(\d{1,3},\d{1,3}\)", line):
+    if get_highest_index_below_value(command.start(), do_cmd) > get_highest_index_below_value(command.start(), dont_cmd):
+        nums = re.findall("[0-9]+", line[command.start(): command.end()])
+        total_enabled += (int(nums[0]) * int(nums[1]))
+    else:
+        nums = re.findall("[0-9]+", line[command.start(): command.end()])
+        total_disabled += (int(nums[0]) * int(nums[1]))
+print(f"Total enabled is :{total_enabled}")   
+print(f"Total disabled is :{total_disabled}")  
+print(f"Total added together is {total_enabled + total_disabled}")
+print(f"We are off by {162813399 - (total_enabled + total_disabled)}") 
 
 # print(total)
 
